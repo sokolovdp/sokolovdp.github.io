5b45515e46a16915c556a24d12d5cb211dc823fc
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-07-29 01:21:01 +0300

    introduced classes for css use, updated var names

---
+++ README.md
@@ -0,0 +1,9 @@
+# Site of the Zen of Python 
+
+At each visit site shows one of 19th Zen of Python statements with random background.
+You can see it here: [Zen of Python](https://sokolovdp.github.io)
+
+# Project Goals
+
+The code is written for educational purposes. 
+Training course for web-developers - [DEVMAN.org](https://devman.org)
--- zen.js
+++ zen.js
@@ -2,28 +2,26 @@ function randomInt(range) {
     return Math.floor((Math.random() * (range - 1)));
 }
 
-function getElement(idName) {
+function getJsonData(idName) {
     var manifest = document.getElementById(idName).innerHTML;
     return JSON.parse(manifest)
 }
 
 function replaceH1Text(zen) {
-    var h1 = document.getElementById("H1");
+    var h1 = document.getElementById("h1");
     h1.innerHTML = zen;
 }
 
 function replaceBodyBackGroundImage(image) {
-    var body = document.getElementById("Body");
+    var body = document.getElementById("body");
     body.setAttribute("background", image)
 }
 
 function makeZen(){
-    var zenArray = getElement("zen_json");
+    var zenArray = getJsonData("zen_json");
     var zenOfDay = zenArray[randomInt(zenArray.length)]["zen"];
-    var imageArray = getElement("zen_images");
+    var imageArray = getJsonData("zen_images");
     var imageOfDay = "./static/" + imageArray[randomInt(imageArray.length)];
-
-    console.log(zenOfDay + " -> " + imageOfDay);
 
     replaceH1Text(zenOfDay);
     replaceBodyBackGroundImage(imageOfDay);
